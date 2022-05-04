e5ce0162d616ed89f2eb7bbf3ed15f0e52aa9ead
Andrei Korneev <andie.kor@yandex.ru>
2022-05-04 19:53:30 +0300

    add 3 task 3 module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('');
+  
+  arr.forEach((elem, index) =>{
+    if (elem === '-'){
+      arr.splice(index, 1);
+      arr[index] = arr[index].toUpperCase();
+    }
+  });
+
+  return arr.join('');
 }
+
+camelize('-background-color-li');
