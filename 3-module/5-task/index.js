0d75ac3cc65c07dea6c6491b772cdfb29b9e93b6
Andrei Korneev <andie.kor@yandex.ru>
2022-05-04 22:10:09 +0300

    add 5 task 3 module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ');
+  let arrOfNumbers = [];
+
+  for (let item of arr){
+    if (!isNaN(+item)) arrOfNumbers.push(+item);
+  }
+  
+  return result = {
+    min: Math.min(...arrOfNumbers),
+    max: Math.max(...arrOfNumbers),
+  };
 }
