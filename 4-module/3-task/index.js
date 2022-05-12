0f6291baf420877086549c2719cb92dbe122063b
Andrei Korneev <andie.kor@yandex.ru>
2022-05-12 08:09:44 +0300

    add 4 module 3 task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,19 @@
 function highlight(table) {
-  // ваш код...
+
+  for (let tr of table.lastElementChild.children){
+
+    let trAvailableFlag = tr.lastElementChild.dataset.available;
+    if (trAvailableFlag == 'true') tr.classList.add('available');
+    else if (trAvailableFlag == 'false') tr.classList.add('unavailable');
+    else tr.hidden = true;
+
+    let gender = tr.cells[2].innerHTML;
+    if (gender == 'm') tr.classList.add('male');
+    else if (gender == 'f') tr.classList.add('female');
+
+    if (+tr.cells[1].innerHTML < 18){
+      tr.style.textDecoration = 'line-through';
+    }
+
+  }
 }
