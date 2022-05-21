2f49e80923fb6c0237f980058d6fd825e71ae13f
Andrei Korneev <andie.kor@yandex.ru>
2022-05-21 14:56:05 +0300

    add 5-module 3 task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,51 @@
 function initCarousel() {
-  // ваш код...
+  let carouselCurrentPosition = 0;
+  let currentSlideWidth = 0;
+  let currentVisibleSlide = 0; 
+
+  const carousel = document.querySelector('.carousel');
+  const carouselInner = document.querySelector('.carousel__inner');
+  const carouselSlides = document.querySelectorAll('.carousel__slide');
+  const carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+    
+  checkDisplayCarousel();
+
+  carousel.addEventListener('click', function(event){
+
+    const carouselArrow = event.target.closest('.carousel__arrow');
+    if (!carouselArrow) return;
+    if (!carousel.contains(carouselArrow)) return;
+
+    currentSlideWidth = carouselSlides[currentVisibleSlide].offsetWidth;
+
+    if (carouselArrow.classList.contains ('carousel__arrow_right')) {
+      carouselCurrentPosition -= currentSlideWidth;
+      currentVisibleSlide += 1;
+    }
+
+    if (carouselArrow.classList.contains ('carousel__arrow_left')) {
+      carouselCurrentPosition += currentSlideWidth;
+      currentVisibleSlide -= 1;
+    }
+
+    carouselInner.style.transform = `translateX(${carouselCurrentPosition}px)`;
+
+    checkDisplayCarousel();
+
+  });
+
+  function checkDisplayCarousel(){
+    if (currentVisibleSlide >= carouselSlides.length-1) {
+      carouselArrowRight.style.display = 'none';
+    }
+    else if (currentVisibleSlide == 0) {
+      carouselArrowLeft.style.display = 'none';
+    }
+    else { 
+      carouselArrowLeft.style.display = '';
+      carouselArrowRight.style.display = '';
+    }
+  }
+
 }
