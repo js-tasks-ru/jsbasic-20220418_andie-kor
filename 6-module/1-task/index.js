149c5a14013a5da993aae44486bb400fae7e50ba
Andrei Korneev <andie.kor@yandex.ru>
2022-07-17 20:01:25 +0300

    add 6 module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,34 @@
  */
 export default class UserTable {
   constructor(rows) {
+    
+    let table = document.createElement('table');
+    this.table = table;
+    table.className = 'tableUsers';
+    table.innerHTML = `<thead><tr>
+    <th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th>
+    </tr></thead>`
+
+    let tbody = document.createElement('tbody');
+    table.append(tbody);
+
+    tbody.innerHTML = rows.map((row) => {
+      return `<tr><td class="userName">${row.name}</td>
+      <td class="userAge">${row.age}</td>
+      <td class="userSalary">${row.salary}</td>
+      <td class="userCity">${row.city}</td>
+      <td class="userButton"><button>X</button></td></tr>`
+    }).join('');
+    
+    table.addEventListener('click', this.onClick);
+  }
+
+  onClick(event) {
+    event.target.closest('tr').remove();
+  }
+
+  get elem() {
+    return this.table;  
   }
 }
+
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,47 @@
+import createElement from '../../assets/lib/create-element.js'
+
 export default class ProductCard {
   constructor(product) {
+
+    let card = createElement(`
+    <div id="holder" class="container_half">
+      <div class="card">
+        <div class="card__top">
+          <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+          <span class="card__price">€${product.price.toFixed(2)}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">Laab kai chicken salad</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    /div>
+    `);
+
+    this.card = card;
+    this.productId = product.id;
+
+    let cardButton = this.card.querySelector('.card__button');
+    cardButton.addEventListener('click', (event) => {
+      this.onClick(event);
+    });
+  }  
+
+  onClick(event) {
+    let productEvent = new CustomEvent('product-add', {
+      detail: this.productId,
+      bubbles: true,
+    });
+
+    this.card.dispatchEvent(productEvent);
+    this.card.addEventListener('product-add', {});
+  }
+
+  get elem() {
+    return this.card;
   }
 }
+
+
