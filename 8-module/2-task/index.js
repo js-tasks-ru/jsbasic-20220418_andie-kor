7cb6465bab24608acc6cac9f01919c5c130b8dfc
Andrei Korneev <andie.kor@yandex.ru>
2022-07-22 15:05:17 +0300

    tag completed

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,36 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    
+  
+
+    this.productGrid = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner">
+        
+        </div>
+      </div>`
+  )
+
+  setTimeout(() => this.productsCardsRender(), 0);
+
+  }
+
+  productsCardsRender() {
+    document.querySelector('.products-grid__inner').innerHTML = '';
+    
+    for (let item of this.products){
+      let newItemElement = new ProductCard(item);
+      console.log(newItemElement);
+      document.querySelector('.products-grid__inner').append(newItemElement.elem);
+    }
+  }
+
+  updateFilter(filters) {
+
+  }
+
+  get elem(){
+    return this.productGrid;
   }
 }
